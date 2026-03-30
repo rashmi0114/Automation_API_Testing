8b5e7eb97c8ef7c0bfee6360e68b46f2fdd7a664
rashmi0114 <rashmi.alagiyawanna98@gmail.com>
2026-03-30 22:42:31 +1300

    update get URL

--- tests/api_tests.spec.js
+++ tests/api_tests.spec.js
@@ -1,5 +1,5 @@
 import {test, expect} from '@playwright/test'
 
 test('API GET Request', async({request})=>{
-    const response = await request.get('https://reqres.in/api/users')
+    const response = await request.get('https://jsonplaceholder.typicode.com/posts')
 })
