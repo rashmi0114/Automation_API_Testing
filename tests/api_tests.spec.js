20cd8426b7077fe6c87ee53cd6b5dacf9f033796
rashmi0114 <rashmi.alagiyawanna98@gmail.com>
2026-03-30 22:57:13 +1300

    commit get request testing

--- tests/api_tests.spec.js
+++ tests/api_tests.spec.js
@@ -2,4 +2,9 @@ import {test, expect} from '@playwright/test'
 
 test('API GET Request', async({request})=>{
     const response = await request.get('https://jsonplaceholder.typicode.com/posts')
+    expect(response.status()).toBe(200)
+    const text = await response.text();
+    expect(text).toContain('sunt')
+    console.log(await response.json());
 })
+
